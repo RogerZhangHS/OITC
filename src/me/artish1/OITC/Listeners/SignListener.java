e84f1a0649456fede59d497be4c4435172fd64ad
RogerZhang <rogerzhang20000126@gmail.com>
2014-08-22 00:23:04 +0800

    完成此文件翻译
    译名列表：
    OITC = 密室死斗

--- src/me/artish1/OITC/Listeners/SignListener.java
+++ src/me/artish1/OITC/Listeners/SignListener.java
@@ -28,13 +28,13 @@ public class SignListener implements Listener{
 		if(e.getBlock().getState() instanceof Sign){
 			Sign sign = (Sign) e.getBlock().getState();
 			if(e.getPlayer().hasPermission("oitc.admin")){
-				if(sign.getLine(0).equalsIgnoreCase(ChatColor.DARK_GRAY + "[" + ChatColor.AQUA.toString() + ChatColor.BOLD.toString() + "OITC" + ChatColor.DARK_GRAY + "]")){
+				if(sign.getLine(0).equalsIgnoreCase(ChatColor.DARK_GRAY + "[" + ChatColor.AQUA.toString() + ChatColor.BOLD.toString() + "密室死斗" + ChatColor.DARK_GRAY + "]")){
 				if(e.getPlayer().isSneaking()){
 					
 					for(Arena arena : Arenas.getArenas()){
 						if(sign.getLine(1).equalsIgnoreCase( ChatColor.BOLD + arena.getName() )){
 							arena.removeSign(e.getBlock().getLocation());
-							OITC.sendMessage(e.getPlayer(), "You have removed a sign from " + ChatColor.DARK_AQUA + arena.getName());
+							OITC.sendMessage(e.getPlayer(), "你移除了一个牌子, 这个牌子来自:" + ChatColor.DARK_AQUA + arena.getName());
 							
 							break;
 						}
@@ -43,12 +43,12 @@ public class SignListener implements Listener{
 				}else{
 					e.setCancelled(true);
 					sign.update(true);
-					OITC.sendMessage(e.getPlayer(), "If you want to break this sign, please sneak + break!");
+					OITC.sendMessage(e.getPlayer(), "如果你想破坏这个牌子, 请在蹲伏的同时破坏这个牌子!");
 				}
 				
 			}
 			}else{
-				if(sign.getLine(0).equalsIgnoreCase(ChatColor.DARK_GRAY + "[" + ChatColor.AQUA.toString() + ChatColor.BOLD.toString() + "OITC" + ChatColor.DARK_GRAY + "]")){
+				if(sign.getLine(0).equalsIgnoreCase(ChatColor.DARK_GRAY + "[" + ChatColor.AQUA.toString() + ChatColor.BOLD.toString() + "密室死斗" + ChatColor.DARK_GRAY + "]")){
 					e.setCancelled(true);
 					sign.update(true);
 				}
@@ -72,21 +72,21 @@ public class SignListener implements Listener{
 	        if (e.getLine(1).equalsIgnoreCase(arena.getName()))
 	        {
 
-	        	e.setLine(0, ChatColor.DARK_GRAY + "[" + ChatColor.AQUA.toString() + ChatColor.BOLD.toString() + "OITC" + ChatColor.DARK_GRAY + "]");
+	        	e.setLine(0, ChatColor.DARK_GRAY + "[" + ChatColor.AQUA.toString() + ChatColor.BOLD.toString() + "密室死斗" + ChatColor.DARK_GRAY + "]");
 	          e.setLine(1, ChatColor.BOLD + arena.getName());
 	          e.setLine(3, ChatColor.BOLD + ""+arena.getPlayers().size() + "/" + arena.getMaxPlayers());
 	          
 	          if (arena.getState() == GameState.INGAME) {
-		          e.setLine(2, ChatColor.DARK_RED + "Ingame");
+		          e.setLine(2, ChatColor.DARK_RED + "正在游戏");
 		        } else {
 		        	if(arena.getState() == GameState.LOBBY){
-		          e.setLine(2, ChatColor.GREEN + "Waiting");
+		          e.setLine(2, ChatColor.GREEN + "等待玩家中");
 		        	}else{
 		        		if(arena.getState() == GameState.STOPPING){
-		      	          e.setLine(2, ChatColor.RED + "Stopping");
+		      	          e.setLine(2, ChatColor.RED + "正在结束");
 		        		}else{
 		        			if(arena.getState() == GameState.STARTING){
-		        		          e.setLine(2, ChatColor.AQUA + "Starting");
+		        		          e.setLine(2, ChatColor.AQUA + "正在开始");
 		        			}
 		        		}
 		        	}
@@ -96,7 +96,7 @@ public class SignListener implements Listener{
 	
 	          arena.addSign(e.getBlock().getLocation());
 	          arena.updateSigns();
-	          player.sendMessage(ChatColor.GRAY + "You made a join sign for " + ChatColor.GOLD + arena.getName());
+	          player.sendMessage(ChatColor.GRAY + "你制作了一个加入牌子, 右击这个牌子可以加入场地: " + ChatColor.GOLD + arena.getName());
 	        }
 	      }
 	    }
@@ -110,13 +110,13 @@ public class SignListener implements Listener{
 		if(e.getAction() == Action.RIGHT_CLICK_BLOCK && e.hasBlock() && e.getClickedBlock().getState()  instanceof Sign){
 			Sign sign = (Sign) e.getClickedBlock().getState();
 				if(sign.getLine(0).equalsIgnoreCase(ChatColor.DARK_GRAY + "[" + ChatColor.AQUA.toString() + ChatColor.BOLD.toString() + 
-			        "OITC" + ChatColor.DARK_GRAY + "]"))
+			        "密室死斗" + ChatColor.DARK_GRAY + "]"))
 				{
 			        sign.update();
 			        
 			        if(Arenas.isInArena(player)){
-			        	player.sendMessage(ChatColor.RED + "You are already in an Arena!");
-			        	player.sendMessage(ChatColor.GRAY + "If you would like to leave the current arena you are in, do /oitc leave");
+			        	player.sendMessage(ChatColor.RED + "你在就在一个场地中了!");
+			        	player.sendMessage(ChatColor.GRAY + "如果你想离开现在所在的场地, 请使用指令 /oitc leave");
 			        	return;
 			        }
 			        
@@ -131,7 +131,7 @@ public class SignListener implements Listener{
 			        					
 			        					
 			        				}else{
-			        					player.sendMessage(ChatColor.RED + "Sorry! That Arena is full!");
+			        					player.sendMessage(ChatColor.RED + "对不起! 那个场地已经满了!");
 			        				}
 			        				
 			        				
